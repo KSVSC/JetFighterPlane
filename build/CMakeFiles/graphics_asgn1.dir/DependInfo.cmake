37ecaf874c871f8ac80683128b02d7a39b79a34f
KSVSC <samhitaexplorica9@gmail.com>
2021-05-20 03:32:35 +0530

    build files added

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/aero.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/aero.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/arrow.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/arrow.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/ball.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/bullet.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/bullet.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/cannon.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/cannon.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/color.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/dashboard.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/dashboard.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/display.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/display.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/drop.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/drop.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/fly_bullet.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/fly_bullet.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/fuel_tank.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/fuel_tank.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/ground.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/ground.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/input.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/main.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/nonedit.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/number.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/number.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/other_handlers.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/parachutes.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/parachutes.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/shoot.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/shoot.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/smoke_ring.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/smoke_ring.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/stroke.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/stroke.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/timer.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/src/volcano.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn2/assgn2/build/CMakeFiles/graphics_asgn1.dir/src/volcano.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
